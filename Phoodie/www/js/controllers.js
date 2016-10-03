da0c9ce1298969ae7551aea78697526506dd7eb7
Christopher Pang <matthewstirbate@gmail.com>
2016-10-03 13:49:48 -0700

    Using Cordova Geolocation plugin
    Now maps shows up on device with correct location

--- Phoodie/www/js/controllers.js
+++ Phoodie/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('phoodie.controllers', [])
 
-.controller('MapCtrl', function($scope, $ionicLoading, mapService, $ionicPopup, $firebaseObject, $ionicModal) {
+.controller('MapCtrl', function($scope, $ionicLoading, mapService, $ionicPopup, $firebaseObject, $ionicModal, $cordovaGeolocation) {
 
   //mapService.init();
   var map;
@@ -15,10 +15,39 @@ angular.module('phoodie.controllers', [])
   $scope.data = $firebaseObject(ref);
 
 
-
-  $scope.testSave = function() {
-    //TESTING HERE ONLY
-  }
+  ionic.Platform.ready(function(){
+    $ionicLoading.show({
+            template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+        });
+         
+        var posOptions = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 0
+        };
+ 
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
+             
+            var myLatlng = new google.maps.LatLng(lat, long);
+             
+            var mapOptions = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };          
+             
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);          
+             
+            $scope.map = map;   
+            $ionicLoading.hide();           
+             
+        }, function(err) {
+            $ionicLoading.hide();
+            console.log(err);
+        });
+    })
 
 
   $scope.initialize = function() {
